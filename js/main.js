251f1447d8b0e435db3e5271bceab915bd3e5849
Ethan Dong <dayiethan@gmail.com>
2023-06-08 09:05:18 -0400

    fixed new modal bug

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["a Student.", "a Robotics Enthusiast.", "an Aspiring Engineer.", "a Swimmer."],
+      strings: ["a Student.", "a Robotics Enthusiast.", "an Aspiring Engineer."],
       typeSpeed: 100,
       loop: true,
     });
